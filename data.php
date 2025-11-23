3545cff20360c63293e599fefe293a018c003c96
pheraxie <pheraxieapp@gmail.com>
2025-11-23 17:10:42 +0300

    clean chapter

--- data.php
+++ data.php
@@ -2,55 +2,6 @@
 header('Content-Type: application/json');
 echo '[
     {
-        "manhwa_id": "manhwa_1763894842685",
-        "manhwa_title": "passion!",
-        "manhwa_cover": ".\\/Image\\/passion.jpg",
-        "manhwa_description": "aa",
-        "manhwa_season": "2",
-        "read_count": 0,
-        "order_index": 2,
-        "last_read_at": null,
-        "date_added": "2025-11-23 11:47:22",
-        "__backendId": "local_1763894842_689129371"
-    },
-    {
-        "manhwa_id": "manhwa_1763889340683",
-        "manhwa_title": "passionol",
-        "manhwa_cover": ".\\/Image\\/passion.jpg",
-        "manhwa_description": "ll",
-        "manhwa_season": "2",
-        "read_count": 0,
-        "order_index": 0,
-        "last_read_at": null,
-        "date_added": "2025-11-23 10:15:40",
-        "__backendId": "local_1763889340_1900519909"
-    },
-    {
-        "manhwa_id": "manhwa_1763837978756",
-        "manhwa_title": "depp in love",
-        "manhwa_cover": ".\\/Image\\/deep.jpg",
-        "manhwa_description": "ee",
-        "manhwa_season": "",
-        "read_count": 0,
-        "order_index": 0,
-        "last_read_at": null,
-        "date_added": "2025-11-22 19:59:38",
-        "__backendId": "local_1763837978_312613870"
-    },
-    {
-        "manhwa_id": "manhwa_1763889340683",
-        "chapter_number": 7,
-        "chapter_title": "ff",
-        "chapter_description": "",
-        "chapter_season": "2",
-        "last_read_at": null,
-        "chapter_pages": ".\\/chapitres\\/qqq\\/6.mhtml",
-        "chapter_cover": "",
-        "is_favorite": true,
-        "date_added": "2025-11-23 11:24:14",
-        "__backendId": "local_1763893454_1756510601"
-    },
-    {
         "type": "tracking",
         "id": "tracking_1763899760918_0.20491773173439431",
         "title": "eee",
@@ -100,24 +51,5 @@ echo '[
             "updated_at": "2025-11-23 10:15:41"
         },
         "deleted_at": "2025-11-23 11:28:10"
-    },
-    {
-        "type": "trash",
-        "trash_type": "chapter",
-        "original_data": {
-            "manhwa_id": "manhwa_1763889340683",
-            "chapter_number": 7,
-            "chapter_title": "ff",
-            "chapter_description": "",
-            "chapter_season": "2",
-            "last_read_at": null,
-            "chapter_pages": ".\\/chapitres\\/qqq\\/6.mhtml",
-            "chapter_cover": "",
-            "is_favorite": true,
-            "date_added": "2025-11-23 11:24:14",
-            "__backendId": "local_1763893454_1756510601"
-        },
-        "deleted_at": "2025-11-23T13:54:24.435Z",
-        "id": "trash_1763906064435_0.5722777601901691"
     }
 ]';
